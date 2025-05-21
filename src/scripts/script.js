c6ca9051215dec03d489ef1c0f110efc5dd7315f
lucasdvenancio <lucas.venancio@grupoboticario.com.br>
2025-05-21 18:34:00 -0300

    Ajustado botões carrosel e imagens do quiz

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -17,11 +17,11 @@ fetch("./questions.csv")
     });
 
 // Scroll suave para as seções
-document.getElementById("scroll-button").addEventListener("click", function() {
-    document.getElementById("definicoes").scrollIntoView({ behavior: "smooth" });
+document.getElementById("scroll-into-definitions").addEventListener("click", function() {
+    document.getElementById("definitions").scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById("scroll-button-2").addEventListener("click", function() {
+document.getElementById("scroll-into-quiz").addEventListener("click", function() {
     document.getElementById("quiz").scrollIntoView({ behavior: "smooth" });
 });
 
@@ -67,3 +67,48 @@ document.querySelector('.next-button').addEventListener('click', nextCard);
 document.querySelector('.prev-button').addEventListener('click', prevCard);
     
 updateCarousel();
+
+// Quiz
+
+quiz = document.getElementById("quiz")
+
+// Espera a função fetch executar antes de criar o card
+function waitForFetch() {
+    return new Promise((resolve) => {
+        const interval = setInterval(() => {
+            if (questions.length > 0) {
+                clearInterval(interval);
+                resolve();
+            }
+        }, 100);
+    });
+}
+waitForFetch().then(() => {
+    // Cria o card
+    create_new_card();
+});
+
+function create_new_card() {
+    // Limpa conteudo do quiz
+    quiz.innerHTML = "<h2>Vamos testar seu conhecimento?</h2>";
+    // Cria um novo card
+    const newCard = document.createElement("div");
+    newCard.className = "quiz-card";
+    // Faz com que o nome da pergunta seja aleatório
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const question = questions[randomIndex].question;
+    const correctAnswer = questions[randomIndex].correctAnswer;
+    const incorrectAnswer = questions[randomIndex].incorrectAnswer;
+    newCard.innerHTML = `
+        <h2>${question}</h2>
+        <div class="quiz-image-container">
+            <img src="./assets/${correctAnswer}.jpg" class="quiz-image">
+            <img src="./assets/${incorrectAnswer}.jpg" class="quiz-image">
+        </div>
+        <button class="card-button" onclick="create_new_card()" id="card-button">Trocar pergunta</button>
+    `
+
+    // Adiciona o card ao quiz
+    quiz.appendChild(newCard);
+    return newCard;
+}
